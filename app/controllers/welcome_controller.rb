32a253a160ac0ef0fccce05582c760932d31e360
Chloe Steffens <caforonda103@yahoo.com>
2020-09-07 20:58:00 +0000

    Automatically backed up by Learn

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,13 @@ class WelcomeController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to homepage_path(@user)
+    else
+      redirect_to '/'
+    end
   end
 
   def show
