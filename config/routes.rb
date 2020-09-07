4637964439c393904a332c1e9f5c8ef2c16f9767
Chloe Steffens <caforonda103@yahoo.com>
2020-09-07 21:03:36 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/', to: 'users#new', as: 'user'
+  post '/', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/:user', to: 'users#show', as: 'homepage'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
