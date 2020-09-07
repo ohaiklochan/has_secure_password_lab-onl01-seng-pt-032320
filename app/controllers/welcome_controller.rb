473b0ec55b0c89fdfa75e0262f8d3377f578e716
Chloe Steffens <caforonda103@yahoo.com>
2020-09-07 20:58:19 +0000

    Automatically backed up by Learn

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,4 +15,11 @@ class WelcomeController < ApplicationController
 
   def show
   end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+  
 end
